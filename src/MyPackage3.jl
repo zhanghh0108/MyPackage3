05e5570bc1003ff3754969ff16178c3bd1fa0d02
zhanghh0108 <zhanghh@tongyuan.cc>
2021-06-22 20:55:30 +0800

    修改接口实现

--- src/MyPackage3.jl
+++ src/MyPackage3.jl
@@ -2,10 +2,11 @@ module MyPackage3
 
 # Write your package code here.
 
-export greet, foo
+export greet, foo, foo2
 
 greet() = println("Hello World!")
 
-foo(x,y) = x+2y
+foo(x,y) = x+y
+foo2(x,y) = x+2y
 
 end
